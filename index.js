69d066023413fb242c0a99185af12d1f661d3aa3
Jpeng <peng8350@gmail.com>
2018-03-23 17:19:15 +0800

    init Packages and add the third party components

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import MainActivity from './jscore/ui/MainActivity';
 
-AppRegistry.registerComponent('Gank', () => App);
+AppRegistry.registerComponent('Gank', () => MainActivity);
+
