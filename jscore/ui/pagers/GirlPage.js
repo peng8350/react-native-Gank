df601089221df9d145411b308e96382db7146ba2
Jpeng <peng8350@gmail.com>
2018-04-08 13:44:01 +0800

    Remove image-viewer dependence(Performance issue)

--- jscore/ui/pagers/GirlPage.js
+++ jscore/ui/pagers/GirlPage.js
@@ -2,7 +2,7 @@
  * @Author: Jpeng
  * @Date: 2018-03-24 22:54:12 
  * @Last Modified by: Jpeng
- * @Last Modified time: 2018-04-02 19:52:42
+ * @Last Modified time: 2018-04-08 13:42:39
  * @Email: peng8350@gmail.com 
  */
 
@@ -11,14 +11,12 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import * as Actions from "../../actions/fetchGirlAction";
 import GirlList from "../../components/list/GirlList";
-import ZoomPicView from "../../components/view/ZoomPicView";
 
 export default class GirlPage extends Component {
   render() {
     return (
       <View>
         {<GirlList />}
-        {<ZoomPicView />}
       </View>
     );
   }
