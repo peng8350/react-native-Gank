8210d44f8397489bbc49728aa6d7dc087553627b
Jpeng <peng8350@gmail.com>
2018-04-18 23:06:37 +0800

    Add Activity RightBtn to show checkbox in list

--- jscore/ui/LikeActivity.js
+++ jscore/ui/LikeActivity.js
@@ -2,17 +2,24 @@
  * @Author: Jpeng 
  * @Date: 2018-04-12 17:19:47 
  * @Last Modified by: Jpeng
- * @Last Modified time: 2018-04-18 20:54:17
+ * @Last Modified time: 2018-04-18 23:05:09
  * @Email: peng8350@gmail.com 
  */
 
 //@flow
 
 import React, { Component } from "react";
-import { FlatList, View, Text } from "react-native";
+import { FlatList, View, Text, Button } from "react-native";
 import { TabNavigator } from "react-navigation";
 import LikePage from "./pagers/LikePage";
-import { THEMECOLOR, TEXTNORMALCOLOR, NIGHTTHEMECOLOR, BOTTTOMBGCOLOR, NIGHTBGCOLOR, NIGHTNORMALTEXTCOLOR } from "../constants/colors";
+import {
+  THEMECOLOR,
+  TEXTNORMALCOLOR,
+  NIGHTTHEMECOLOR,
+  BOTTTOMBGCOLOR,
+  NIGHTBGCOLOR,
+  NIGHTNORMALTEXTCOLOR
+} from "../constants/colors";
 import ScrollableTabView, {
   ScrollableTabBar
 } from "react-native-scrollable-tab-view";
@@ -22,43 +29,138 @@ class LikeActivity extends Component {
   constructor() {
     super();
     this.state = {
-      selectTab: 0,
-      isDeleting: false
+      rightBtnText: "删除"
     };
   }
 
-  //md-checkmark
-  static navigationOptions = {
-    headerTitle: "收藏"
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: "收藏",
+    headerRight: (<Button color='#fff' title={navigation.state.params
+      ?navigation.state.params.rightText:'删除'} onPress={navigation.state.params
+        ?navigation.state.params.pressRight:() => {}}/>),
+        
+    
+  });
+
+  changeRightText =  (state) => {
+    switch(state){
+      case 0:
+      this.setState({
+        rightBtnText: '删除'
+      });
+      this.props.navigation.setParams({
+        rightText: '删除',
+      });
+      break;
+      case 1:
+      this.setState({
+        rightBtnText: '取消'
+      });
+      this.props.navigation.setParams({
+        rightText: '取消',
+      });
+      break;
+      case 2:
+      this.setState({
+        rightBtnText: '确定'
+      });
+      this.props.navigation.setParams({
+        rightText: '确定',
+      });
+      break;
+      default:
+
+      break;
+    }
+  }
+
+  _onPressRight = () => {
+    if(this.state.rightBtnText=== "删除"){
+      this.changeRightText(1)
+    }
+    else{
+      if(this.state.rightBtnText=== "确定"){
+
+      }
+      else{
+        
+      }
+
+      this.changeRightText(0);
+    }
   };
+
+  componentWillMount() {
+    this.props.navigation.setParams({
+      rightText: this.state.rightBtnText,
+      pressRight: this._onPressRight
+    });
+  }
 
   render() {
     return (
       <ScrollableTabView
-        style={{backgroundColor: this.props.isNight?BOTTTOMBGCOLOR:'#f3f3f3'}}
-        tabBarBackgroundColor={this.props.isNight?NIGHTBGCOLOR:"white"}
-        tabBarUnderlineStyle={{ backgroundColor: this.props.isNight?NIGHTTHEMECOLOR:THEMECOLOR }}
-        tabBarInactiveTextColor={this.props.isNight?NIGHTNORMALTEXTCOLOR:TEXTNORMALCOLOR}
+        style={{
+          backgroundColor: this.props.isNight ? BOTTTOMBGCOLOR : "#f3f3f3"
+        }}
+        tabBarBackgroundColor={this.props.isNight ? NIGHTBGCOLOR : "white"}
+        tabBarUnderlineStyle={{
+          backgroundColor: this.props.isNight ? NIGHTTHEMECOLOR : THEMECOLOR
+        }}
+        tabBarInactiveTextColor={
+          this.props.isNight ? NIGHTNORMALTEXTCOLOR : TEXTNORMALCOLOR
+        }
         initialPage={0}
-        tabBarActiveTextColor={this.props.isNight?NIGHTTHEMECOLOR:THEMECOLOR}
+        tabBarActiveTextColor={
+          this.props.isNight ? NIGHTTHEMECOLOR : THEMECOLOR
+        }
         renderTabBar={() => <ScrollableTabBar />}
       >
-        <LikePage tabLabel="前端" type="前端" isDeleting ={this.state.isDeleting}/>
-        <LikePage tabLabel="Android" type="Android" isDeleting ={this.state.isDeleting} />
-        <LikePage tabLabel="IOS" type="iOS" isDeleting ={this.state.isDeleting} />
-        <LikePage tabLabel="App" type="App" isDeleting ={this.state.isDeleting} />
-        <LikePage tabLabel="瞎推荐" type="瞎推荐" isDeleting ={this.state.isDeleting} />
-        <LikePage tabLabel="拓展资源" type="拓展资源" isDeleting ={this.state.isDeleting} />
+        <LikePage
+          tabLabel="前端"
+          {...this.state}
+          change ={this.changeRightText}
+          type="前端"
+        />
+        <LikePage
+          tabLabel="Android"
+          {...this.state}
+          change ={this.changeRightText}
+          type="Android"
+        />
+        <LikePage
+          tabLabel="IOS"
+          {...this.state}
+          change ={this.changeRightText}
+          type="iOS"
+        />
+        <LikePage
+          tabLabel="App"
+          {...this.state}
+          change ={this.changeRightText}
+          type="App"
+        />
+        <LikePage
+          tabLabel="瞎推荐"
+          {...this.state}
+          change ={this.changeRightText}
+          type="瞎推荐"
+        />
+        <LikePage
+          tabLabel="拓展资源"
+          {...this.state}
+          change ={this.changeRightText}
+          type="拓展资源"
+        />
       </ScrollableTabView>
     );
   }
 }
 
-
 const stateToProps = state => {
   return {
     isNight: state.SettingReducer.isNight
-  }
-}
+  };
+};
 
-export default connect(stateToProps)(LikeActivity)
+export default connect(stateToProps)(LikeActivity);
--- jscore/ui/pagers/LikePage.js
+++ jscore/ui/pagers/LikePage.js
@@ -2,7 +2,7 @@
  * @Author: Jpeng 
  * @Date: 2018-04-12 17:23:55 
  * @Last Modified by: Jpeng
- * @Last Modified time: 2018-04-18 22:27:55
+ * @Last Modified time: 2018-04-18 23:05:58
  * @Email: peng8350@gmail.com 
  */
 //@flow
@@ -12,14 +12,55 @@ import { View, Text, FlatList } from "react-native";
 import GankItem from "../../components/Item/GankItem";
 import GankManager from "../../utils/GankManager";
 import MyCheckBox from "../../components/view/MyCheckBox";
+import { FETCH_GIRL_SUCCESS } from "../../actions/ActionType";
 
 export default class LikePage extends Component {
+  //用来判断选中的数量
+  selectCount = 0;  
   constructor() {
     super();
     this.state = {
       dataSource: [],
       selectList: []
     };
+  }
+
+  _renderCheckBox(index){
+    if(this.props.rightBtnText!=='删除'){
+      return (
+        <View
+        style={{
+          flex: 1,
+          padding: 5,
+          position: "absolute",
+          alignItems: "flex-end",
+          bottom: 0,
+          top: 0,
+          right: 0,
+          left: 0
+        }}
+      >
+        <MyCheckBox selected={this.state.selectList[index]} onChecked={(selected) => {
+          this.selectCount += selected?1:-1;
+          this.state.selectList[index] = selected
+          if(this.selectCount>0)
+          this.props.change(2)
+          else this.props.change(1)
+          this.setState({
+            selectList: [].concat(this.state.selectList)
+          })
+        }}/>
+      </View>
+      )
+    }
+    return null;
+  }
+
+  clearCount(){
+    for (var item of this.state.queryList) {
+      item = false
+    }
+    this.selectCount=0
   }
 
   _renderItem = ({ item,index}) => {
@@ -36,25 +77,7 @@ export default class LikePage extends Component {
             this.props.navigation.navigate("Web", { url: item.url });
           }}
         />
-        <View
-          style={{
-            flex: 1,
-            padding: 5,
-            position: "absolute",
-            alignItems: "flex-end",
-            bottom: 0,
-            top: 0,
-            right: 0,
-            left: 0
-          }}
-        >
-          <MyCheckBox selected={this.state.selectList[index]} onChecked={(selected) => {
-            this.state.selectList[index] = selected
-            this.setState({
-              selectList: [].concat(this.state.selectList)
-            })
-          }}/>
-        </View>
+        {this._renderCheckBox(index)}
       </View>
     );
   };
@@ -64,7 +87,7 @@ export default class LikePage extends Component {
       <FlatList
         data={this.state.dataSource}
         keyExtractor={(item, index) => index + ""}
-        extraData={this.state.selectList}
+        extraData={[this.state.selectList,this.state.rightBtnText]}
         renderItem={this._renderItem}
       />
     );
